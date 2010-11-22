ccc262ca68315ab870c7a7eed4088b0eea450e54
xixsimplicityxix <xixsimplicityxix@548b1a3e-7590-2476-7de9-fbf5bcc5e58c>
2010-11-22 17:27:37 +0000

    Check for the proper amount of arguments before trying to use them. Add a usage message. This fixes an out of bounds exception.

--- src/CompressAddressSpace.java
+++ src/CompressAddressSpace.java
@@ -4,20 +4,24 @@ import java.util.Scanner;
 
 public class CompressAddressSpace {
 	public static void main(String args[]) throws FileNotFoundException {
-		Scanner in = new Scanner(new File(args[0]));
-		long maxAddress = 0xFFFFFFFF;
-		if (args.length == 2) {
-			maxAddress = Long.parseLong(args[1], 16);
-		}
-
-		while (in.hasNext() == true) {
-			String type = in.next();
-			String direction = in.next();
-			long address = in.nextLong(16);
-
-			address = address % maxAddress;
-
-			System.out.printf("%s\t%s\t%x\n", type, direction, address);
+		if (args.length < 1) {
+			System.out.println("Usage: java CompressAddressSpace <TRACEFILE>");
+		} else {
+			Scanner in = new Scanner(new File(args[0]));
+			long maxAddress = 0xFFFFFFFF;
+			if (args.length == 2) {
+				maxAddress = Long.parseLong(args[1], 16);
+			}
+	
+			while (in.hasNext() == true) {
+				String type = in.next();
+				String direction = in.next();
+				long address = in.nextLong(16);
+	
+				address = address % maxAddress;
+	
+				System.out.printf("%s\t%s\t%x\n", type, direction, address);
+			}
 		}
 	}
 
